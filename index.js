9850144b9142408b2edb6754044b1f15cc5569fb
christianmaldonado <christian.zldr@gmail.com>
2022-04-16 23:15:29 -0400

    commit tree 9c495b473369b040242a055363b1e6385f3e05f4

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+//find the element
+//remove it
+document.getElementById("main").remove();
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.innerHTML = "YOUR-NAME is the champion"
